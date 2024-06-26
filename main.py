e84c7dd3aa42e9a3c784d4160b57fc6e499fd98f
Lee Phil Seung <sisyphus2020@naver.com>
2024-06-26 11:11:11 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -1,16 +1,15 @@
-# from dotenv import load_dotenv
-# load_dotenv()
+# import os
 import streamlit as st
-import time
+# from dotenv import load_dotenv
 from langchain_openai import OpenAI
+
+# load_dotenv()
 
 llm = OpenAI()
 
 st.title('수학문제 풀이')
 content = st.text_input("수학 문제를 제시해주세요.", "")
 
-
-result = ''
 if st.button("문제 풀이"):
     with st.spinner('문제 풀이 중...'):
         result = llm.invoke(content + "에 대한 수학 문제를 풀어줘")
